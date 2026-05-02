8fbeb935be53c88bd8fe91f88e0d916c640953b3
Piyush <piyush9c24@gmail.com>
2026-05-02 13:44:07 +0530

    change in server.js

--- server.js
+++ server.js
@@ -165,6 +165,6 @@ function getOnlineInRoom(room) {
 
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
-});
+app.get("/", (req, res) => res.send("Backend is running!"));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
